6e8b062a718019c0d9ca50d34af7030ce5a222e7
Dkathayat <deepaksingh1990@yahoo.com>
2024-04-17 14:12:55 +0530

    commit2

--- app/src/main/java/com/kathayat/imagecaching/ui/presention/ImageHomeScreen.kt
+++ app/src/main/java/com/kathayat/imagecaching/ui/presention/ImageHomeScreen.kt
@@ -1,7 +1,8 @@
 package com.kathayat.imagecaching.ui.presention
 
 import android.util.Log
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -9,6 +10,7 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -18,64 +20,68 @@ import androidx.paging.compose.collectAsLazyPagingItems
 import com.kathayat.imagecaching.network.remote.ImagesDtoItem
 import com.kathayat.imagecaching.utils.ErrorMessage
 import com.kathayat.imagecaching.utils.LoadingNextPageItem
-import com.kathayat.imagecaching.utils.PageLoader
 import com.kathayat.imagecaching.vm.ImageViewModel
 
 @Composable
 fun ImageHomeScreen(
     viewModel: ImageViewModel = hiltViewModel()
-){
-    val imagesPagingItems: LazyPagingItems<ImagesDtoItem> = viewModel.imagesState.collectAsLazyPagingItems()
-    LazyVerticalGrid(
+) {
+    val imagesPagingItems: LazyPagingItems<ImagesDtoItem> =
+        viewModel.imagesState.collectAsLazyPagingItems()
 
-        columns = GridCells.Fixed(2),
-        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        itemsIndexed(imagesPagingItems.itemSnapshotList.items){index, item ->
-            Spacer(modifier = Modifier.padding(4.dp))
-            ImageItem(
-                urls = imagesPagingItems[index]!!.urls
-            )
-            Log.d("ImageUrls", imagesPagingItems[index]!!.urls.toString())
-        }
-//        item { Spacer(modifier = Modifier.padding(4.dp)) }
-////        items(imagesPagingItems.itemCount) { index ->
-////            ImageItem(
-////                urls = imagesPagingItems[index]!!.urls
-////            )
-////        }
-        imagesPagingItems.apply {
-            when {
-                loadState.refresh is LoadState.Loading -> {
-                    item { PageLoader(modifier = Modifier.fillMaxSize()) }
-                }
 
-                loadState.refresh is LoadState.Error -> {
-                    val error = imagesPagingItems.loadState.refresh as LoadState.Error
-                    item {
-                        ErrorMessage(
-                            modifier = Modifier.fillMaxSize(),
-                            message = error.error.localizedMessage!!,
-                            onClickRetry = { retry() })
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalArrangement = Arrangement.Center,
+        ) {
+            itemsIndexed(imagesPagingItems.itemSnapshotList.items) { index, item ->
+                Spacer(modifier = Modifier.padding(4.dp))
+                ImageItem(
+                    urls = imagesPagingItems[index]!!.urls
+                )
+                Log.d("ImageUrls", imagesPagingItems[index]!!.urls.toString())
+            }
+            imagesPagingItems.apply {
+                when {
+                    loadState.refresh is LoadState.Loading -> {
+                        item {  }
+
                     }
-                }
 
-                loadState.append is LoadState.Loading -> {
-                    item { LoadingNextPageItem(modifier = Modifier) }
-                }
+                    loadState.refresh is LoadState.Error -> {
+                        val error = imagesPagingItems.loadState.refresh as LoadState.Error
+                        item {
+                            ErrorMessage(
+                                modifier = Modifier.fillMaxSize(),
+                                message = error.error.localizedMessage!!,
+                                onClickRetry = { retry() })
+                        }
+                    }
 
-                loadState.append is LoadState.Error -> {
-                    val error = imagesPagingItems.loadState.append as LoadState.Error
-                    item {
-                        ErrorMessage(
-                            modifier = Modifier,
-                            message = error.error.localizedMessage!!,
-                            onClickRetry = { retry() })
+                    loadState.append is LoadState.Loading -> {
+                        item { LoadingNextPageItem(modifier = Modifier) }
+                    }
+
+                    loadState.append is LoadState.Error -> {
+                        val error = imagesPagingItems.loadState.append as LoadState.Error
+                        item {
+                            ErrorMessage(
+                                modifier = Modifier,
+                                message = error.error.localizedMessage!!,
+                                onClickRetry = { retry() })
+                        }
                     }
                 }
             }
+            // item { Spacer(modifier = Modifier.padding(4.dp)) }
         }
-        item { Spacer(modifier = Modifier.padding(4.dp)) }
     }
-
 }
+
--- app/src/main/java/com/kathayat/imagecaching/ui/presention/ImageIitem.kt
+++ app/src/main/java/com/kathayat/imagecaching/ui/presention/ImageIitem.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
@@ -27,7 +26,7 @@ fun ImageItem(urls: Urls) {
     ) {
         AsyncImage(
            model = urls.regular,
-            modifier = Modifier.size(200.dp)
+            modifier = Modifier.fillMaxSize()
                 .padding(20.dp)
                 .clip(RoundedCornerShape(10)),
             contentDescription = null,
